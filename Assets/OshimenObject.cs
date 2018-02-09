9ae5fe5d1a38acfaa718d51a6a57c6eac65f7859
Isao Kawaguchi <isa130pull@yahoo.co.jp>
2018-02-10 00:13:09 +0900

    推しメン操作

---
+++ Assets/OshimenObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using GodTouches;
+
+public class OshimenObject : MonoBehaviour {
+
+	private void SetGravity()
+	{
+		GetComponent<Rigidbody2D>().isKinematic = false;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    // タッチを検出して動かす
+		var phase = GodTouch.GetPhase ();
+		if (phase == GodPhase.Began) 
+		{
+		}
+		else if (phase == GodPhase.Moved)
+		{
+			var touchPos = Camera.main.ScreenToWorldPoint(GodTouch.GetPosition());
+			transform.position = new Vector3(touchPos.x,transform.position.y);
+		}
+		else if (phase == GodPhase.Ended) 
+		{
+			SetGravity();
+		}
+	}
+}
