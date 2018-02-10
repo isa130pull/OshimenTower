6846a62244b7633b1761bea0cca3369ae53b0024
Isao Kawaguchi <isa130pull@yahoo.co.jp>
2018-02-10 09:26:18 +0900

    Photonインポート

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UniRx.Triggers;
+using GodTouches;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour {
+
+	[SerializeField]
+	private Button rotateButton;
+
+	[SerializeField]
+	private SpriteRenderer cloudImage;
+
+	private GameObject oshimen;
+
+	private bool isMyTurn = true;
+
+	void Awake() {
+		Application.targetFrameRate = 60;
+	}
+
+	void Start () {
+		CreateOshimen();
+		this.rotateButton.OnClickAsObservable()
+			.Subscribe(_ =>
+			{
+				Debug.Log("Click");
+				if (this.oshimen)
+				{
+					Debug.Log("oshimen not null");
+					var currentRotate = this.oshimen.transform.rotation.eulerAngles.z;
+					this.oshimen.transform.DORotate(new Vector3(0,0,currentRotate + 45f),0.2f)
+						.SetEase(Ease.Linear);
+				}
+			});
+		
+		this.UpdateAsObservable()
+			.Where(_ => oshimen)
+			.Where(__ => oshimen.GetComponent<OshimenObject>().IsGround)
+			.Subscribe(_ =>
+			{
+				CreateOshimen();
+			});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveCloud();
+
+	}
+
+	private void MoveCloud()
+	{
+		var cloudPos = this.cloudImage.transform.position;
+		if (cloudPos.x <= - 3.8f)
+		{
+			cloudPos.x = 4.0f;
+		}
+		this.cloudImage.transform.position = new Vector3(cloudPos.x - 0.005f, cloudPos.y);
+	}
+
+	private void CreateOshimen()
+	{
+		var oshimen = Resources.Load("oshimenObject") as GameObject;		
+		this.oshimen = Instantiate(oshimen);
+	}
+
+
+}
---
+++ Assets/Scripts/OshimenObject.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using GodTouches;
+using UnityEngine.EventSystems;
+using UniRx;
+using System;
+
+public class OshimenObject : MonoBehaviour {
+
+	public bool IsGround = false;
+	private bool isTouch = false;
+	private void SetGravity(bool isKinematic)
+	{
+		GetComponent<Rigidbody2D>().isKinematic = isKinematic;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Mathf.Abs(transform.position.x) > 4.0f || Mathf.Abs(transform.position.y) > 8.0f)
+		{
+			Debug.Log("Destroy");
+			Destroy(gameObject);
+			return;
+		}
+
+		var isKinematic = GetComponent<Rigidbody2D>().isKinematic;
+		bool isPointer;
+		#if UNITY_EDITOR
+			isPointer = EventSystem.current.IsPointerOverGameObject();
+		#else
+			isPointer = EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId);
+		#endif
+
+		if(isPointer || !isKinematic || this.IsGround){
+            return;
+        }
+
+	    // タッチを検出して動かす
+		var phase = GodTouch.GetPhase ();
+		if (phase == GodPhase.Began) 
+		{
+			this.isTouch = true;
+		}
+		else if (phase == GodPhase.Moved && this.isTouch)
+		{			
+			var touchPos = Camera.main.ScreenToWorldPoint(GodTouch.GetPosition());
+			transform.position = new Vector3(touchPos.x,transform.position.y);
+		}
+		else if (phase == GodPhase.Ended && this.isTouch) 
+		{
+			SetGravity(false);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		Observable.Timer(TimeSpan.FromMilliseconds(1000f))
+			.Subscribe(_ =>
+			{
+				this.IsGround = true;
+			});
+	}
+	void OnCollisionExit2D(Collision2D coll) {
+	}
+
+
+}
