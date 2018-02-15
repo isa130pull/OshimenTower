971d45bd5c8b088048539895d0dce08d23a02333
Isao Kawaguchi <isa130pull@yahoo.co.jp>
2018-02-16 07:28:56 +0900

    OshimenObjectメソッドをリファクタリング

--- Assets/Scripts/OshimenObject.cs
+++ Assets/Scripts/OshimenObject.cs
@@ -12,22 +12,18 @@ public class OshimenObject : MonoBehaviour {
 	public bool IsGround = false;
 	private bool isTouch = false;
 
-//	[PunRPC]
-	private void SetGravity(bool isKinematic)
+	private int targetRotate = 0;
+
+	// 45度アニメーション付きで回転させる
+	public void Rotate45()
 	{
-		GetComponent<Rigidbody2D>().isKinematic = isKinematic;
+		PhotonView.Get(this).RPC("RpcRotate", PhotonTargets.All);
 	}
 
-	// Use this for initialization
-	void Start () {
-		if (!GameManager.IsMaster)
-		{
-			// gameObject.GetComponent<PolygonCollider2D>().isTrigger = true;
-		}		
-	}
-	
 	// Update is called once per frame
-	void Update () {
+	private void Update () {
+		// オブジェクトが画面外に出た時
+		// TODO: オブジェクト削除ではなく、ゲームオーバー判定に用いる
 		if (Mathf.Abs(transform.position.x) > 4.0f || Mathf.Abs(transform.position.y) > 8.0f)
 		{
 			Debug.Log("Destroy");
@@ -35,18 +31,30 @@ public class OshimenObject : MonoBehaviour {
 			return;
 		}
 
-		var isKinematic = GetComponent<Rigidbody2D>().isKinematic;
-		bool isPointer;
+		bool isButtonTouches;
+		// ボタンタップ中か否か
 		#if UNITY_EDITOR
-			isPointer = EventSystem.current.IsPointerOverGameObject();
+			isButtonTouches = EventSystem.current.IsPointerOverGameObject();
 		#else
-			isPointer = EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId);
+			isButtonTouches = EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId);
 		#endif
+		var isKinematic = GetComponent<Rigidbody2D>().isKinematic;
 
-		if(isPointer || !isKinematic || this.IsGround || !GameManager.IsMyTurn){
+		// 自キャラを操作できない条件
+		// ①回転ボタンタップ中
+		// ②重力設定中(自由落下中)
+		// ③落下済
+		// ④相手ターン
+		if (isButtonTouches || !isKinematic || this.IsGround || !GameManager.IsMyTurn){
             return;
         }
 
+		TouchProcess();
+	}
+
+	// タップ中の操作やタップエンド処理
+	private void TouchProcess()
+	{
 	    // タッチを検出して動かす
 		var phase = GodTouch.GetPhase ();
 		if (phase == GodPhase.Began) 
@@ -56,32 +64,60 @@ public class OshimenObject : MonoBehaviour {
 		else if (phase == GodPhase.Moved && this.isTouch)
 		{			
 			var touchPos = Camera.main.ScreenToWorldPoint(GodTouch.GetPosition());
-			transform.position = new Vector3(touchPos.x,transform.position.y);
-			Debug.Log(name);
+			PhotonView.Get(this).RPC("RpcPosition", PhotonTargets.All,touchPos);
 		}
 		else if (phase == GodPhase.Ended && this.isTouch) 
 		{
-			PhotonView photonView = PhotonView.Get(this);
-			SetGravity(false);
+			// 物理判定を適用させる(重力による自由落下含)
+			PhotonView.Get(this).RPC("RpcSetKinematic", PhotonTargets.All,false);
+			this.isTouch = false;
 		}
 	}
 
-	void OnCollisionEnter2D(Collision2D coll) {
+	// 他オブジェクトに当たった瞬間呼ばれる
+	private void OnCollisionEnter2D(Collision2D coll) {
 		Observable.Timer(TimeSpan.FromMilliseconds(1000f))
 			.Subscribe(_ =>
 			{
-				PhotonView photonView = PhotonView.Get(this);
-				photonView.RPC("SetIsGround", PhotonTargets.All);
+				// 他オブジェクトに当たって一定時間経過 = 着地した判定
+				// TODO: オブジェクトの動きが止まったら着地判定
+				PhotonView.Get(this).RPC("RpcIsGround", PhotonTargets.All);
 			});
 	}
+
+	// 他オブジェクトへの接触処理が落ち着いたら呼ばれる
 	void OnCollisionExit2D(Collision2D coll) {
 	}
 
+
+	// RPC経由でIsGroundフラグをTrueに共有させる
 	[PunRPC]
-	void SetIsGround()
+	void RpcIsGround()
 	{
-		Debug.Log("SetIsGround");
 		this.IsGround = true;
 	}
+
+	// RPC経由で操作キャラを45度回転させる
+	[PunRPC]
+	private void RpcRotate()
+	{
+		this.targetRotate += 45;
+		this.transform.DORotate(new Vector3(0,0,this.targetRotate),0.2f)
+			.SetEase(Ease.Linear);
+	}
+
+	// RPC経由で操作中キャラのPositionを同期させる
+	[PunRPC]
+	private void RpcPosition(Vector3 touchPos)
+	{
+		transform.position = new Vector3(touchPos.x,transform.position.y);
+	}
+
+	// RPC経由でKinematicのフラグを同期させる
+	private void RpcSetKinematic(bool isKinematic)
+	{
+		GetComponent<Rigidbody2D>().isKinematic = isKinematic;
+	}
+
 
 }
