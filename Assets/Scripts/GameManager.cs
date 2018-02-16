dad3dcb26f1e2ec17ec67872058289ea72fbb94c
Isao Kawaguchi <isa130pull@yahoo.co.jp>
2018-02-17 07:38:50 +0900

    namespaceを設定

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,137 +6,142 @@ using UniRx;
 using UniRx.Triggers;
 using GodTouches;
 using DG.Tweening;
+using Tower.Object;
 
-public class GameManager : MonoBehaviour {
+namespace Tower
+{
+	public class GameManager : MonoBehaviour {
 
-	// 自分のターンかどうかのフラグ
-	public static bool IsMyTurn = false;
+		// 自分のターンかどうかのフラグ
+		public static bool IsMyTurn = false;
 
-	// 部屋のマスター権限を持っているかのフラグ
-	public static bool IsMaster = false;
+		// 部屋のマスター権限を持っているかのフラグ
+		public static bool IsMaster = false;
 
-	[SerializeField]
-	private Button rotateButton;
+		[SerializeField]
+		private Button rotateButton;
 
-	[SerializeField]
-	private SpriteRenderer cloudImage;
+		[SerializeField]
+		private SpriteRenderer cloudImage;
 
-	[SerializeField]
-	private Text debugText;
+		[SerializeField]
+		private Text debugText;
 
-	[SerializeField]
-	private SpriteRenderer myTurnText;
+		[SerializeField]
+		private SpriteRenderer myTurnText;
 
-	private GameObject oshimen;
+		private GameObject oshimen;
 
-	void Awake() {
-		Application.targetFrameRate = 60;
+		void Awake() {
+			Application.targetFrameRate = 60;
 
-		// OnRaiseEventをコールバック登録
-        PhotonNetwork.OnEventCall += OnRaiseEvent;
-	}
-
-	void Start () {
-		// Photonに接続する(引数でゲームのバージョンを指定できる)
-        PhotonNetwork.ConnectUsingSettings(null);
-		this.myTurnText.enabled = false;
-
-		// 回転ボタン押下時
-		this.rotateButton.OnClickAsObservable()
-			.Where(_ => IsMyTurn && this.oshimen.GetComponent<Rigidbody2D>().isKinematic)
-			.Subscribe(_ =>
-			{
-				// 45度回転させる
-				this.oshimen.GetComponent<OshimenObject>().Rotate45();
-			});
-		
-		// オブジェクトが着地したかどうかをチェックする
-		this.UpdateAsObservable()
-			.Where(_ => this.oshimen)
-			.Where(__ => this.oshimen.GetComponent<OshimenObject>().IsGround)
-			.Subscribe(_ =>
-			{
-				Debug.Log("ターンエンド");
-				// 自分のターンを終え、相手のターンに移行
-				IsMyTurn = this.myTurnText.enabled = false;
-				this.oshimen = null;
-				//TODO: 現在は１件しか飛ばすものがないので引数は仮値
-				PhotonNetwork.RaiseEvent((byte)1, "TurnEnd", true, RaiseEventOptions.Default );
-			});
-	}
-
-	
-	// Update is called once per frame
-	void Update () {
-		MoveCloud();
-	}
-
-	// 雲の移動
-	private void MoveCloud()
-	{
-		var cloudPos = this.cloudImage.transform.position;
-		// 画面左端まで行ったら右端に移動させる
-		if (cloudPos.x <= - 3.8f)
-		{
-			cloudPos.x = 4.0f;
+			// OnRaiseEventをコールバック登録
+			PhotonNetwork.OnEventCall += OnRaiseEvent;
 		}
-		this.cloudImage.transform.position = new Vector3(cloudPos.x - 0.005f, cloudPos.y);
-	}
 
-	// オブジェクトを生成
-	private void CreateOshimen()
-	{
-		var oshimen = Resources.Load("oshimenObject") as GameObject;
-		this.oshimen = PhotonNetwork.Instantiate("oshimenObject",oshimen.transform.position,Quaternion.identity,0);
+		void Start () {
+			// Photonに接続する(引数でゲームのバージョンを指定できる)
+			PhotonNetwork.ConnectUsingSettings(null);
+			this.myTurnText.enabled = false;
+
+			// 回転ボタン押下時
+			this.rotateButton.OnClickAsObservable()
+				.Where(_ => this.oshimen)
+				.Where(_ => IsMyTurn && this.oshimen.GetComponent<Rigidbody2D>().isKinematic)
+				.Subscribe(_ =>
+				{
+					// 45度回転させる
+					this.oshimen.GetComponent<OshimenObject>().Rotate45();
+				});
+			
+			// オブジェクトが着地したかどうかをチェックする
+			this.UpdateAsObservable()
+				.Where(_ => this.oshimen)
+				.Where(__ => this.oshimen.GetComponent<OshimenObject>().IsGround)
+				.Subscribe(_ =>
+				{
+					Debug.Log("ターンエンド");
+					// 自分のターンを終え、相手のターンに移行
+					IsMyTurn = this.myTurnText.enabled = false;
+					this.oshimen = null;
+					//TODO: 現在は１件しか飛ばすものがないので引数は仮値
+					PhotonNetwork.RaiseEvent((byte)1, "TurnEnd", true, RaiseEventOptions.Default );
+				});
+		}
+
 		
-		IsMyTurn = this.myTurnText.enabled = true;
+		// Update is called once per frame
+		void Update () {
+			MoveCloud();
+		}
+
+		// 雲の移動
+		private void MoveCloud()
+		{
+			var cloudPos = this.cloudImage.transform.position;
+			// 画面左端まで行ったら右端に移動させる
+			if (cloudPos.x <= - 3.8f)
+			{
+				cloudPos.x = 4.0f;
+			}
+			this.cloudImage.transform.position = new Vector3(cloudPos.x - 0.005f, cloudPos.y);
+		}
+
+		// オブジェクトを生成
+		private void CreateOshimen()
+		{
+			var oshimen = Resources.Load("oshimenObject") as GameObject;
+			this.oshimen = PhotonNetwork.Instantiate("oshimenObject",oshimen.transform.position,Quaternion.identity,0);
+			
+			IsMyTurn = this.myTurnText.enabled = true;
+		}
+
+		// ロビーに入ると呼ばれる
+		void OnJoinedLobby() {
+			string message = "ロビーに入りました。";
+			Debug.Log(message);
+			debugText.text = message;
+	
+			// ルームに入室する
+			PhotonNetwork.JoinRandomRoom();
+		}
+	
+		// ルームに入室すると呼ばれる
+		void OnJoinedRoom() {
+			string message = "ルームへ入室しました。";
+			debugText.text = message;
+			Debug.Log(message);
+		}
+	
+		// ルームの入室に失敗すると呼ばれる
+		void OnPhotonRandomJoinFailed() {
+			string message = "ルームの入室に失敗しました。";
+			debugText.text = message;
+			Debug.Log(message);
+	
+			// ルームがないと入室に失敗するため、その時は自分で作る
+			// 引数でルーム名を指定できる
+			PhotonNetwork.CreateRoom("myRoomName");
+		}
+
+		void OnPhotonPlayerConnected( PhotonPlayer newPlayer ) {
+			string message = "誰かがルームに入室しました。";
+			debugText.text = message;
+			Debug.Log(message);
+
+			// 他の誰かが入ってきた = 自身がマスター判定
+			IsMaster = IsMyTurn = true;
+			CreateOshimen();
+		}
+
+		private void OnRaiseEvent( byte i_eventcode, object i_content, int i_senderid )
+		{
+			// ターンが変更した時に呼ばれる
+			Debug.Log("OnRaiseEvent");
+			this.oshimen = null;
+			CreateOshimen();
+		}
+
+
 	}
-
-	// ロビーに入ると呼ばれる
-    void OnJoinedLobby() {
-		string message = "ロビーに入りました。";
-        Debug.Log(message);
-		debugText.text = message;
- 
-        // ルームに入室する
-        PhotonNetwork.JoinRandomRoom();
-    }
- 
-    // ルームに入室すると呼ばれる
-    void OnJoinedRoom() {
-		string message = "ルームへ入室しました。";
-		debugText.text = message;
-        Debug.Log(message);
-    }
- 
-    // ルームの入室に失敗すると呼ばれる
-    void OnPhotonRandomJoinFailed() {
-		string message = "ルームの入室に失敗しました。";
-		debugText.text = message;
-        Debug.Log(message);
- 
-        // ルームがないと入室に失敗するため、その時は自分で作る
-        // 引数でルーム名を指定できる
-        PhotonNetwork.CreateRoom("myRoomName");
-    }
-
-	void OnPhotonPlayerConnected( PhotonPlayer newPlayer ) {
-		string message = "誰かがルームに入室しました。";
-		debugText.text = message;
-        Debug.Log(message);
-
-		// 他の誰かが入ってきた = 自身がマスター判定
-		IsMaster = IsMyTurn = true;
-		CreateOshimen();
-	}
-
-	 private void OnRaiseEvent( byte i_eventcode, object i_content, int i_senderid )
-    {
-        // ターンが変更した時に呼ばれる
-		Debug.Log("OnRaiseEvent");
-		this.oshimen = null;
-		CreateOshimen();
-    }
-
-
 }
--- Assets/Scripts/OshimenObject.cs
+++ Assets/Scripts/OshimenObject.cs
@@ -8,127 +8,130 @@ using UniRx;
 using UniRx.Triggers;
 using System;
 
-public class OshimenObject : MonoBehaviour {
+namespace Tower.Object
+{
+	public class OshimenObject : MonoBehaviour {
 
-	public bool IsGround = false;
-	private bool isTouch = false;
+		public bool IsGround = false;
+		protected bool isTouch = false;
 
-	private int targetRotate = 0;
+		protected int targetRotate = 0;
 
-	// 45度アニメーション付きで回転させる
-	public void Rotate45()
-	{
-		PhotonView.Get(this).RPC("RpcRotate", PhotonTargets.All);
-	}
+		// 45度アニメーション付きで回転させる
+		public void Rotate45()
+		{
+			PhotonView.Get(this).RPC("RpcRotate", PhotonTargets.All);
+		}
 
-	private void Start()
-	{
-		// オブジェクトが画面外に出た時
-		// TODO: オブジェクト削除ではなく、ゲームオーバー判定に用いる
-		this.UpdateAsObservable()
-			.Where(_ => Mathf.Abs(transform.position.x) > 4.0f || Mathf.Abs(transform.position.y) > 8.0f && this.IsGround)
-			.Subscribe(_ =>
-			{
-				Debug.Log("DestroyObject");
-				Destroy(gameObject);
+		private void Start()
+		{
+			// オブジェクトが画面外に出た時
+			// TODO: オブジェクト削除ではなく、ゲームオーバー判定に用いる
+			this.UpdateAsObservable()
+				.Where(_ => Mathf.Abs(transform.position.x) > 4.0f || Mathf.Abs(transform.position.y) > 8.0f && this.IsGround)
+				.Subscribe(_ =>
+				{
+					Debug.Log("DestroyObject");
+					Destroy(gameObject);
+					return;
+				});
+		}
+
+		private void Update () 
+		{
+			bool isButtonTouches;
+			// ボタンタップ中か否か
+			#if UNITY_EDITOR
+				isButtonTouches = EventSystem.current.IsPointerOverGameObject();
+			#else
+				isButtonTouches = EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId);
+			#endif
+			var isKinematic = GetComponent<Rigidbody2D>().isKinematic;
+
+			// 自キャラを操作できない条件
+			// ①回転ボタンタップ中
+			// ②重力設定中(自由落下中)
+			// ③落下済
+			// ④相手ターン
+			if (isButtonTouches || !isKinematic || this.IsGround || !GameManager.IsMyTurn){
 				return;
-			});
-	}
+			}
 
-	private void Update () 
-	{
-		bool isButtonTouches;
-		// ボタンタップ中か否か
-		#if UNITY_EDITOR
-			isButtonTouches = EventSystem.current.IsPointerOverGameObject();
-		#else
-			isButtonTouches = EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId);
-		#endif
-		var isKinematic = GetComponent<Rigidbody2D>().isKinematic;
+			TouchProcess();
+		}
 
-		// 自キャラを操作できない条件
-		// ①回転ボタンタップ中
-		// ②重力設定中(自由落下中)
-		// ③落下済
-		// ④相手ターン
-		if (isButtonTouches || !isKinematic || this.IsGround || !GameManager.IsMyTurn){
-            return;
-        }
-
-		TouchProcess();
-	}
-
-	// タップ中の操作やタップエンド処理
-	private void TouchProcess()
-	{
-	    // タッチを検出して動かす
-		var phase = GodTouch.GetPhase ();
-		var touchPos = Camera.main.ScreenToWorldPoint(GodTouch.GetPosition());
-		if (phase == GodPhase.Began) 
+		// タップ中の操作やタップエンド処理
+		private void TouchProcess()
 		{
-			this.isTouch = true;
-		}
-		else if (phase == GodPhase.Moved && this.isTouch)
-		{			
-			PhotonView.Get(this).RPC("RpcPosition", PhotonTargets.All,touchPos);
-		}
-		else if (phase == GodPhase.Ended && this.isTouch) 
-		{
-			// 物理判定を適用させる(重力による自由落下含)
-			PhotonView.Get(this).RPC("RpcSetKinematic", PhotonTargets.All, touchPos, false);
-			this.isTouch = false;
-		}
-	}
-
-	// 他オブジェクトに当たった瞬間呼ばれる
-	private void OnCollisionEnter2D(Collision2D coll) {
-		Observable.Timer(TimeSpan.FromMilliseconds(1000f))
-			.Where(_ => !this.IsGround)
-			.Subscribe(_ =>
+			// タッチを検出して動かす
+			var phase = GodTouch.GetPhase ();
+			var touchPos = Camera.main.ScreenToWorldPoint(GodTouch.GetPosition());
+			if (phase == GodPhase.Began) 
 			{
-				// 他オブジェクトに当たって一定時間経過 = 着地した判定
-				// TODO: オブジェクトの動きが止まったら着地判定
-				PhotonView.Get(this).RPC("RpcIsGround", PhotonTargets.All);
-			});
+				this.isTouch = true;
+			}
+			else if (phase == GodPhase.Moved && this.isTouch)
+			{			
+				PhotonView.Get(this).RPC("RpcPosition", PhotonTargets.All,touchPos);
+			}
+			else if (phase == GodPhase.Ended && this.isTouch) 
+			{
+				// 物理判定を適用させる(重力による自由落下含)
+				PhotonView.Get(this).RPC("RpcSetKinematic", PhotonTargets.All, touchPos, false);
+				this.isTouch = false;
+			}
+		}
+
+		// 他オブジェクトに当たった瞬間呼ばれる
+		private void OnCollisionEnter2D(Collision2D coll) {
+			Observable.Timer(TimeSpan.FromMilliseconds(1000f))
+				.Where(_ => !this.IsGround)
+				.Subscribe(_ =>
+				{
+					// 他オブジェクトに当たって一定時間経過 = 着地した判定
+					// TODO: オブジェクトの動きが止まったら着地判定
+					PhotonView.Get(this).RPC("RpcIsGround", PhotonTargets.All);
+				});
+		}
+
+		// 他オブジェクトへの接触処理が落ち着いたら呼ばれる
+		void OnCollisionExit2D(Collision2D coll) {
+		}
+
+
+		// RPC経由でIsGroundフラグをTrueに共有させる
+		[PunRPC]
+		void RpcIsGround()
+		{
+			this.IsGround = true;
+		}
+
+		// RPC経由で操作キャラを45度回転させる
+		[PunRPC]
+		private void RpcRotate()
+		{
+			this.targetRotate += 45;
+			this.transform.DORotate(new Vector3(0,0,this.targetRotate),0.2f)
+				.SetEase(Ease.Linear);
+		}
+
+		// RPC経由で操作中キャラのPositionを同期させる
+		[PunRPC]
+		private void RpcPosition(Vector3 touchPos)
+		{
+			transform.position = new Vector3(touchPos.x,transform.position.y);
+		}
+
+		// RPC経由でKinematicのフラグを同期させる
+		[PunRPC]
+		private void RpcSetKinematic(Vector3 touchPos, bool isKinematic)
+		{
+			// ポジションを同期
+			transform.position = new Vector3(touchPos.x,transform.position.y);
+			// Kinematicフラグを同期
+			GetComponent<Rigidbody2D>().isKinematic = isKinematic;
+		}
+
 	}
-
-	// 他オブジェクトへの接触処理が落ち着いたら呼ばれる
-	void OnCollisionExit2D(Collision2D coll) {
-	}
-
-
-	// RPC経由でIsGroundフラグをTrueに共有させる
-	[PunRPC]
-	void RpcIsGround()
-	{
-		this.IsGround = true;
-	}
-
-	// RPC経由で操作キャラを45度回転させる
-	[PunRPC]
-	private void RpcRotate()
-	{
-		this.targetRotate += 45;
-		this.transform.DORotate(new Vector3(0,0,this.targetRotate),0.2f)
-			.SetEase(Ease.Linear);
-	}
-
-	// RPC経由で操作中キャラのPositionを同期させる
-	[PunRPC]
-	private void RpcPosition(Vector3 touchPos)
-	{
-		transform.position = new Vector3(touchPos.x,transform.position.y);
-	}
-
-	// RPC経由でKinematicのフラグを同期させる
-	[PunRPC]
-	private void RpcSetKinematic(Vector3 touchPos, bool isKinematic)
-	{
-		// ポジションを同期
-		transform.position = new Vector3(touchPos.x,transform.position.y);
-		// Kinematicフラグを同期
-		GetComponent<Rigidbody2D>().isKinematic = isKinematic;
-	}
-
 
 }
